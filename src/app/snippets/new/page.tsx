15a3ea9eef7891f379c0d97133132f31b7c0326c
Britta Rodenbeck <cheers@brittarodenbeck.com>
2023-11-29 11:14:13 -0600

    :sparkles: creating FE create form

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,27 @@
+export default function CreateSnippet() {
+  return (
+    <form>
+      <h1 className="font-bold m3-">Create a Snippet</h1>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">Title</label>
+          <input
+            className="border rounded p-2 w-full"
+            id="title"
+            name="title"
+            type="text"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">Code</label>
+          <textarea
+            className="border rounded p-2 w-full"
+            id="code"
+            name="code"
+          />
+        </div>
+        <button className="rounded p-2 bg-blue-200" type="submit">Create</button>
+      </div>
+    </form>
+  );
+}
