90dad215d905173d9c824b210dfc00516d11b17d
Britta Rodenbeck <cheers@brittarodenbeck.com>
2023-11-30 14:56:59 -0600

    :lipstick: styling snippets index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
 import { db } from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
-  return snippets.map((snippet) => (
-    <p key={snippet.id}>
-      {snippet.title}
-    </p>
-  ));
+  return (
+    <>
+      <header className="flex justify-between m-2 items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link
+          href="/snippets/new"
+          className="border p-2 rounded"
+        >
+          New
+        </Link>
+      </header>
+      <div className="flex flex-col gap-2">
+        {snippets.map((snippet) => (
+          <div
+            className="flex justify-between items-center p-2 border rounded"
+            key={snippet.id}
+          >
+            <p key={snippet.id}>
+              {snippet.title}
+            </p>
+            <Link href={`/snippets/${snippet.id}`}>View</Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
