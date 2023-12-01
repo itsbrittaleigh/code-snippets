a5b72dd76e28fcfa5d793bcc36bce47e59c94191
Britta Rodenbeck <cheers@brittarodenbeck.com>
2023-12-01 13:11:18 -0600

    :sparkles: adding error handling when creating a snippet

--- src/actions/snippets.ts
+++ src/actions/snippets.ts
@@ -24,27 +24,39 @@ export async function getSnippetById(id: number | string) {
 }
 
 export async function createSnippet(formState: IFormState, formData: FormData) {
-  const title = formData.get('title') as string;
-  const code = formData.get('code') as string;
-
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Title must be longer.'
-    };
+  try {
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+  
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer.'
+      };
+    }
+  
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer.'
+      };
+    }
+  
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message
+      }
+    } else {
+      return {
+        message: 'Something went wrong!'
+      }
+    }
   }
-
-  if (typeof code !== 'string' || code.length < 10) {
-    return {
-      message: 'Code must be longer.'
-    };
-  }
-
-  await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
 
   redirect('/');
 }
